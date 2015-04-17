f1f548e212bd4fa4ac08719493987a0a7478e13a
DevInCube <ruslan.hadyniak@gmail.com>
2015-04-17 15:40:59 +0300

    localization first tests pass

--- VitML.JsonSchemaViewModels/Localization/SchemaLocalizationData.cs
+++ VitML.JsonSchemaViewModels/Localization/SchemaLocalizationData.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -18,9 +19,32 @@ namespace VitML.JsonVM.Localization
 
         public static SchemaLocalizationData Parse(string json)
         {
-            JArray array = JArray.Parse(json);
+            JObject obj = JObject.Parse(json);
 
-            return new SchemaLocalizationData();
+            var data = new SchemaLocalizationData();
+            foreach (var prop in obj)
+            {
+                var langDict = new Dictionary<string, string>();
+                data.langData[prop.Key] = langDict;
+                JObject lang = prop.Value as JObject;
+                foreach (var pair in lang)
+                {
+                    langDict[pair.Key] = (pair.Value as JValue).Value<string>();
+                }            
+            }
+            return data;
+        }
+
+        public string GetString(string key, CultureInfo info)
+        {
+            if (langData.ContainsKey(info.Name))
+            {
+                if (langData[info.Name].ContainsKey(key))
+                {
+                    return langData[info.Name][key];
+                }
+            }
+            return null;
         }
     }
 }
